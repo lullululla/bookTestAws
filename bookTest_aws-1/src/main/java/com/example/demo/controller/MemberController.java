fb3142e936ff6bfb1c80ddd0cef946724b1ca103
xo-hyun <xo_hyunn@naver.com>
2024-03-22 13:59:41 +0900

    test

--- bookTest_aws-1/src/main/java/com/example/demo/controller/MemberController.java
+++ bookTest_aws-1/src/main/java/com/example/demo/controller/MemberController.java
@@ -2,9 +2,12 @@ package com.example.demo.controller;
 
 import java.util.Optional;
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,45 +25,31 @@ import lombok.Setter;
 public class MemberController {
    @Autowired
    private MemberService ms;
+   
    @Autowired
-   private MemberDAO MemberDAO;
+   private MemberDAO memberDAO;
 
    @GetMapping("/member/login")
    public void loginForm() {      
    }
-   
-//   @PostMapping("/member/login")
-//   public String loginProcess(@RequestParam("id") String id, 
-//                              @RequestParam("pwd") String pwd) {
-//       // 여기서는 간단히 사용자명과 비밀번호를 콘솔에 출력하는 예제를 보여줍니다.
-//       System.out.println("Username: " + id);
-//       System.out.println("Password: " + pwd);
-//       
-//       // 실제로는 로그인 처리 로직이 이곳에 들어가야 합니다.
-//       // 예를 들어, 데이터베이스에서 사용자 정보를 조회하고 인증을 수행하는 등의 작업이 여기에서 이루어질 수 있습니다.
-//       
-//       // 로그인 처리 후에는 보통 다른 페이지로 리다이렉트하거나 적절한 응답을 반환합니다.
-//       return "redirect:/book/list"; // 로그인 성공 후 홈 페이지로 리다이렉트
-//   }
-   
    @PostMapping("/member/login")
-	public ModelAndView loginSubmit(String id, String pwd, HttpSession session) {
-		String msg = "";
-		ModelAndView mav = new ModelAndView("redirect:/book/list");
-		Optional<Member> option = MemberDAO.findById(id);
-		if(option.isPresent()) {
-			String dbPwd = option.get().getPwd();
-			if(pwd.equals(dbPwd)) {
-				session.setAttribute("id", id);				
-			}else {				
-				mav.setViewName("redirect:/member/login");
-			}
-		}else {			
-			mav.setViewName("redirect:/member/login");
-		}
-		
-		return mav;
-	}
+   public ModelAndView loginSubmit(String id, String pwd, HttpSession session) {
+       ModelAndView mav = new ModelAndView();
+       Optional<Member> optionalMember = memberDAO.findById(id);
+       if (optionalMember.isPresent()) {
+           Member member = optionalMember.get();
+           if (pwd.equals(member.getPwd())) {
+               session.setAttribute("id", id);
+               mav.setViewName("redirect:/book/list");
+           } else {
+               mav.setViewName("redirect:/member/login");
+           }
+       } else {
+           mav.setViewName("redirect:/member/login");
+       }
+       return mav;
+   }
+   
    
    @GetMapping("/member/join")
    public void joinForm() {      
--- bookTest_aws-1/src/main/java/com/example/demo/dao/MemberDAO.java
+++ bookTest_aws-1/src/main/java/com/example/demo/dao/MemberDAO.java
@@ -1,5 +1,8 @@
 package com.example.demo.dao;
 
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,5 @@ import com.example.demo.entity.Member;
 
 @Repository
 public interface MemberDAO extends JpaRepository<Member, String> {
-      
+	public Member findByIdAndPwd(String id, String pwd);
 }
